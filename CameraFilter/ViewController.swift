f9e11a4ed7aa2ab21064d30918a3baf35f98d915
Irina Lapteva <iralapteva44@gmail.com>
2020-06-24 23:46:57 +0300

    Initial Commit

---
+++ CameraFilter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CameraFilter
+//
+//  Created by Ира on 24.06.2020.
+//  Copyright © 2020 Irina Lapteva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
