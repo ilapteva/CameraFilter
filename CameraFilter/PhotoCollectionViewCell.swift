7c1387015799eb365b00e1d241d32bcb63780714
Irina Lapteva <iralapteva44@gmail.com>
2020-07-25 12:05:30 +0300

    Add two VC, get access to photo library, get selected photo on the main VC from Collection View using RxSwift.

---
+++ CameraFilter/PhotoCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoCollectionViewCell.swift
+//  CameraFilter
+//
+//  Created by Ира on 25.07.2020.
+//  Copyright © 2020 Irina Lapteva. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+}
